e38923c968f56125b5e375ebfc84e8491ebcf9a5
vinhp1545@gmail.com <vinh_hdh@MSI.localdomain>
2025-12-09 00:03:21 +0700

    xong phan 1 va 2

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void print_pagetable(pagetable_t pagetable, int level)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      for(int j = 0; j < level; j++) printf(".. ");
+
+      uint64 pa = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, pa);
+
+      // check if leaf
+      if((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+        // non-leaf PTE → recurse
+        pagetable_t child = (pagetable_t)pa;
+        print_pagetable(child, level + 1);
+      }
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  print_pagetable(pagetable, 1);
+}
